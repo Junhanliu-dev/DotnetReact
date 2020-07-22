7b441829322e698d2849a1911136ff30dc7f0f76
junhanliu <junhanliu.leon@gmail.com>
2020-07-22 17:01:40 +1000

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            //Anything unside using is going to be cleaned up automatically after completed.
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try{
+                    var context = services.GetRequiredService<DataContext>();
+
+                    //Applies any pending migrations for the context to the database. 
+                    //Will create the database if it does not already exist.
+                    context.Database.Migrate();
+                }
+                catch(Exception ex){
+
+                    //Inside ILogger, provide Class that will be logged
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
